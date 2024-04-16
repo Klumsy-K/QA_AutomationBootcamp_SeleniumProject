2b77a273ebcb7c3bda5472c04f0249be1d9cc31a
Klumsy K <konstantin.pogorelov@gmail.com>
2024-04-16 15:48:32 -0700

    multiplesOf and multiTable KP 04/16/24

--- src/test/java/Challenges/Challenges2.java
+++ src/test/java/Challenges/Challenges2.java
@@ -1,6 +1,7 @@
 package Challenges;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Challenges2 {
 
@@ -8,6 +9,11 @@ public class Challenges2 {
 
         String revSentence = masterYodaSays("I study WebDriver");
         System.out.println(revSentence);
+
+//      multiplesOf(5);
+
+        multiTable();
+
 
     }
 
@@ -24,7 +30,20 @@ public class Challenges2 {
 
        }
        return Arrays.toString(sentArrRev).replace(",", "");
+    }
 
+//    This method just does the multiplication and presents the results
+    private static void multiplesOf (int num){
+        for (int i = 1; i <= 10; i ++){
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+
+    //        This method asks the user what number he wants to multiply and calls multiplesOf to do its thing..
+    public static void multiTable (){
+        Scanner userInt = new Scanner(System.in);
+        System.out.println("Input integer to multiply: ");
+        multiplesOf(userInt.nextInt());
     }
 
 }
