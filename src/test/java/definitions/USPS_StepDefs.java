25b7d2c353160413ef82c686e420c80bdeb395e6
KlumsyK <konstantin.pogorelov@gmail.com>
2024-04-18 19:13:15 -0700

    USPS scenarios 2 and 3 plus USPS stepdefs 04/16/24

--- src/test/java/definitions/USPS_StepDefs.java
+++ src/test/java/definitions/USPS_StepDefs.java
@@ -5,6 +5,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 import static support.DriverFactory.getDriver;
 
@@ -45,5 +50,60 @@ public class USPS_StepDefs {
         Thread.sleep(250);
        System.out.println(getDriver().findElement(By.xpath("//ul[contains (@class,'list-group')]")).getText());
        Assertions.assertThat(getDriver().findElement(By.xpath("//ul[contains (@class,'list-group')]")).getText()).contains(zipCode);
+    }
+
+    @When("I go to Calculate Price Page # click Send and Calculate a Price for it")
+    public void iGoToCalculatePricePageClickSendAndCalculateAPriceForIt() {
+
+        getDriver().findElement(By.xpath("//a[@class='nav-first-element menuitem']")).click();
+        getDriver().findElement(By.xpath
+                        ("//li/a[@href='https://postcalc.usps.com/']/img[contains (@src,'calculate_price')]"))
+                .click();
+        getDriver().manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+
+    }
+
+    @And("I select destination {string} with {string} shape")
+    public void iSelectDestinationWithShape(String destination, String packageType) {
+
+        getDriver().findElement(By.xpath("//select[@id='CountryID']")).click();
+
+        Actions keyInput = new Actions(getDriver());
+        keyInput.sendKeys(destination).keyDown(Keys.RETURN).keyUp(Keys.RETURN).build().perform();
+
+        switch (packageType){
+            case ("Postcard")  -> getDriver().findElement(By.xpath("//input[@id='option_1']")).click();
+        }
+
+        getDriver().manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+    }
+
+    @Then("I verify for quantity of {string} total cost is {string}")
+    public void iVerifyForQuantityOfTotalCostIs(String quantity, String expectedCost) {
+
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(quantity);
+
+        getDriver().findElement(By.xpath("//input[@value='Calculate'][@type='button']")).click();
+
+        WebElement cost = getDriver().findElement(By.xpath("//div[@id='total']"));
+        Assertions.assertThat(cost.getText().equals(expectedCost));
+    }
+
+    @When("I click {string} under Jobs with USPS")
+    public void iClickUnderJobsWithUSPS(String textLabel) {
+
+        switch (textLabel) {
+         case ("Find Out More") ->  getDriver().findElement(By.xpath("//a[contains(text(),'Find Out More')]")).click();
+        }
+
+        getDriver().manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+
+    }
+
+    @Then("I verify {string} message on the page")
+    public void iVerifyMessageOnThePage(String expectedMessage) {
+        Assertions.assertThat(getDriver().getPageSource().contains(expectedMessage));
+
+
     }
 }
--- src/test/resources/features/USPS.feature
+++ src/test/resources/features/USPS.feature
@@ -7,3 +7,24 @@
       When I go to Lookup Zip page by address
       And I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
       Then I validate "94022" zip code exists in the result
+
+#    @ShowAlertBox
+#    Scenario: Show and confirm alert box.
+#      Given I open url "https://testpages.eviltester.com/styled/index.html"
+#      And I click on element with xpath "//a[@id='alerttest']"
+#      And I click on element with xpath "//input[@id='confirmexample']"
+#      And I wait for 2 sec
+#      And I confirm box alert
+#      And I wait for 2 sec
+    @USPS2
+    Scenario: Price Calculator verification
+      Given I go to "USPS" page
+      When I go to Calculate Price Page # click Send and Calculate a Price for it
+      And I select destination "Canada" with "Postcard" shape
+      Then I verify for quantity of "2" total cost is "$3.10"
+
+    @USPS3
+    Scenario: "Now Hiring" message verification
+      Given I go to "USPS" page
+      When I click "Find Out More" under Jobs with USPS
+      Then I verify "Now hiring" message on the page
