fd54733c49793729a82781deca39a182a69bdc5a
KlumsyK <konstantin.pogorelov@gmail.com>
2024-05-19 10:54:17 -0700

    04/16/24

--- src/test/java/definitions/CalculatorStepDefs.java
+++ src/test/java/definitions/CalculatorStepDefs.java
@@ -1,6 +1,39 @@
 package definitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+import static support.DriverFactory.getDriver;
+
 public class CalculatorStepDefs {
+    @And("I calculate")
+    public void andICalculate (){
+    }
 
+    @When("I navigate to {string}")
+    public void iNavigateTo(String pageChoice) {
+        switch (pageChoice){
+            case "Inflation Calculator":
+                getDriver().findElement(By.xpath("//a[contains(text(),'Inflation Calculator')]"));
+                break;
+        }
+    }
 
+    @And("I clear all values")
+    public void iClearAllValues() {
+    }
+
+    @Then("I verify {string}")
+    public void iVerify(String arg0) {
+    }
+
+    @When("I enter {string} dollars from month of {string} of year {string} to month of {string} year {string}")
+    public void iEnterDollarsFromMonthOfOfYearToMonthOfYear(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("I verify {string} is the buying power")
+    public void iVerifyIsTheBuyingPower(String arg0) {
+    }
 }
--- src/test/resources/features/calculator.feature
+++ src/test/resources/features/calculator.feature
@@ -2,6 +2,22 @@
   @calculator
   Feature: testing the calculator.net functionality
 
-    @calculator1
-    Scenario: Verify calculator results
+    @BG
+      Background:
       Given I go to "calculator" page
+      When I navigate to "Inflation Calculator"
+      And I clear all values
+
+    @calculator1
+    Scenario Outline: Verify calculator results
+
+      Then I verify "Please provide a positive starting amount"
+      When I enter "<$ Amount>" dollars from month of "<Start Month>" of year "<Start Year>" to month of "<End Month>" year "<End Year>"
+      And I calculate
+      Then I verify "277.70" is the buying power
+      Examples:
+        | $ Amount | Start Month| Start Year| End Month| End Year |
+        |   200    |            |   2011    |  March   |   2024   |
+        |   1300   |   April    |   2018    |  April   |   2020   |
+        |   2100   |  Average   |   1989    | January  |   2001   |
+        |  3699.99 |    July    |   1974    | September|   2012   |
